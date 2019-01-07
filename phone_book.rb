3548c6e1320ece96b1047fc0e980009bb160e9fa
shivkolli <shivkolli18@gmail.com>
2019-01-07 23:02:51 +0530

    Create phone_book.rb

---
+++ phone_book.rb
@@ -0,0 +1,8 @@
+class PhoneBook
+  
+  def get_words_from_numbers(numbers)
+    letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],
+    "6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
+  end
+  
+end
